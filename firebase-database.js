e1a83569905188ddea99d78af0bfc3ced8f1a63d
Vil Ostreiko <vil_ostreiko@epam.com>
2019-05-26 23:09:06 +0300

    Update fb keys

--- firebase-database.js
+++ firebase-database.js
@@ -2,13 +2,13 @@
 
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./vendors/credentials/recipes-app-388be-firebase-adminsdk-lkgp5-183657e3ee.json');
+const serviceAccount = require('./vendors/credentials/ng-recipes-5b053-firebase-adminsdk-b7b9x-01a2f02942');
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://recipes-app-388be.firebaseio.com"
+  databaseURL: "https://ng-recipes-5b053.firebaseio.com"
 });
 
 console.log("Start firebase database connection");
-console.log("databaseURL: https://recipes-app-388be.firebaseio.com");
+console.log("databaseURL: https://ng-recipes-5b053.firebaseio.com");
 module.exports = firebase;
